6082344aa5dd8d40d40122ff9bdf221fda2ec956
Racknar0 <racknarow1@gmail.com>
2022-04-15 18:39:10 -0500

    Añadir Primeros Componentes

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <div className='md:w-1/2 lg:w-2/5'>
+      <h2 className='font-black text-xl text-center'>Seguimiento Pacientes</h2>
+      <p className='text-lg mt-5 text-center'>Añade pacientes y {' '} <span className='text-indigo-600 font-bold'>Administralos</span></p>
+    </div>
+  )
+}
+
+export default Formulario
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,13 @@
+
+
+const ListadoPacientes = () => {
+  return (
+
+      <div className="w-1/2 lg:w-3/5">
+          <h2>Listado Pacientes</h2>
+      </div>
+
+  )
+}
+
+export default ListadoPacientes
