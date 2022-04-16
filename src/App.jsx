0ea30b8df588c8485e96aff5136c358d7392a59d
Racknar0 <racknarow1@gmail.com>
2022-04-15 22:17:52 -0500

    Pasando Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import Formulario from './components/Formulario'
 import Header from './components/Header'
 import ListadoPacientes from './components/ListadoPacientes'
@@ -5,9 +7,17 @@ import ListadoPacientes from './components/ListadoPacientes'
 
 function App() {
 
+  const [pacientes, setPacientes] = useState([])
+
+  const tomaUnValor = (valor) => {
+      console.log(valor);
+  }
+
   return (
     <div className='container mx-auto mt-20'>
-      <Header />
+      <Header 
+        tomaUnValor={tomaUnValor}
+      />
 
       <div className='mt-12 md:flex'>
         <Formulario />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({tomaUnValor}) => {
+
+    const variable = true;
+
+    tomaUnValor(variable);
+
+
     return (
             <h1 className='font-black text-4xl text-center md:w-2/3 mx-auto'>Seguimiento Pacientes{' '}<span className='text-indigo-600'>Veterinaria</span></h1>
     );
