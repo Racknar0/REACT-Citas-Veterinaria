b844a40502f23e02ac37ca55f54689b078c8c0e2
Racknar0 <racknarow1@gmail.com>
2022-04-17 16:46:29 -0500

    Añadir botones eliminar y editar

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -11,6 +11,10 @@ const Paciente = ({paciente}) => {
               <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}<span className="font-normal normal-case">{email}</span></p>
               <p className="font-bold mb-3 text-gray-700 uppercase">Fecha alta:{' '}<span className="font-normal normal-case">{fecha}</span></p>
               <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{' '}<span className="font-normal normal-case">{sintomas}</span></p>
+              <div className="flex justify-around mt-10">
+                <button type='button' className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg">Editar</button>
+                <button type='button' className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">Eliminar</button>
+              </div>
     </div>
   )
 }
